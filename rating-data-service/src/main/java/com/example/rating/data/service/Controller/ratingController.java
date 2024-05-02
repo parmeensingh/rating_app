bd52be2597324f0f9421c44f3c4b082f3d95c554
Parmeen Singh <parmeensingh35@gmail.com>
2024-05-03 00:12:08 +0530

    logging added-3rdmay

--- rating-data-service/src/main/java/com/example/rating/data/service/Controller/ratingController.java
+++ rating-data-service/src/main/java/com/example/rating/data/service/Controller/ratingController.java
@@ -1,5 +1,8 @@
 package com.example.rating.data.service.Controller;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.rating.data.service.model.ratingModel;
 
+
 @RestController
 @RequestMapping("/rating")
 public class ratingController {
+	Logger logger = LogManager.getLogger(getClass());
+	Exception e= new RuntimeException("this is first error");
 	@GetMapping("/details/{id}")	
 	public ratingModel getInfo(@PathVariable String id) {
+		logger.info("first log");
+		logger.error("first error");
 		return new ratingModel(21,"godzilla");
 	}
 }
